fcdd7e445b93693bf1aa5909cfe4bd7d5541d841
Rudi Boeke <r.boeke@me.com>
2020-02-27 11:07:42 +0100

    Dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, except: [:new, :create]
+
+  get 'pages/show_dashboard', to: 'pages#show_dashboard', as: :dashboard
 end
